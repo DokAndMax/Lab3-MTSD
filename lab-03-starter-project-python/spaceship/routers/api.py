c7e1dc815abce43d85fb03fc475ac4f4665c2e3d
Maks <maxim.kryvenok@gmail.com>
2023-04-04 05:13:17 +0300

    Implement matrix product endpoint

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter
 
+from numpy import matmul, random
+
 router = APIRouter()
 
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def processMatrix() -> dict:
+    matrix_a = random.rand(10, 10)
+    matrix_b = random.rand(10, 10)
+    return {
+        'matrix_a': matrix_a.tolist(),
+        'matrix_b': matrix_b.tolist(),
+        'product': matmul(matrix_a, matrix_b).tolist()
+    }
